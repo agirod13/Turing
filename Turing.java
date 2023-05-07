31a76050f669bd3c51f2b859ececbe7de4fe6b5b
agirod13 <agirod13@gmail.com>
2023-05-07 12:23:42 -0400

    Added support to check inputs and restart program if input invalid

--- Turing.java
+++ Turing.java
@@ -1,60 +1,78 @@
 import java.util.Scanner;
 public class Turing {
 
-    public static void main(String[] args ) throws InterruptedException {
-
-        System.out.println("        ##      ## ######## ##        ######   #######  ##     ## ########    ########  #######     ######## ##     ## ########  #### ##    ##  ######");   
-        System.out.println("        ##  ##  ## ##       ##       ##    ## ##     ## ###   ### ##             ##    ##     ##       ##    ##     ## ##     ##  ##  ###   ## ##    ##");  
-        System.out.println("        ##  ##  ## ##       ##       ##       ##     ## #### #### ##             ##    ##     ##       ##    ##     ## ##     ##  ##  ####  ## ##");        
-        System.out.println("        ##  ##  ## ######   ##       ##       ##     ## ## ### ## ######         ##    ##     ##       ##    ##     ## ########   ##  ## ## ## ##   ####"); 
-        System.out.println("        ##  ##  ## ##       ##       ##       ##     ## ##     ## ##             ##    ##     ##       ##    ##     ## ##   ##    ##  ##  #### ##    ##");  
-        System.out.println("        ##  ##  ## ##       ##       ##    ## ##     ## ##     ## ##             ##    ##     ##       ##    ##     ## ##    ##   ##  ##   ### ##    ##");  
-        System.out.println("        ###  ###  ######## ########  ######   #######  ##     ## ########       ##     #######        ##     #######  ##     ## #### ##    ##  ######");
-        System.out.println("    *********************************************************************************************************************************************************");
-        System.out.println("");
-        System.out.println("");   
-        System.out.println("");
-        Thread.sleep(1500);
-
-        System.out.println("Do you want to encrypt or decrypt a message?");
-        System.out.println("1 - Encrypt");   
-        System.out.println("2 - Decrypt");
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        Thread.sleep(1000);
-        System.out.println("Enter Selection: ");
-        int selection = myObj.nextInt();
-       
-        
-        if (selection == 1) {
-            Thread.sleep(750);
-            System.out.println("Your selection is: encrypt");
-        } else if (selection == 2) {
-            Thread.sleep(750);
-            System.out.println("Your selection is: decrypt");
-        } else {
-            Thread.sleep(750);
-            System.out.println("Your selection is invalid");
-        }
-        Thread.sleep(2000);
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();  
-   
-
-        
-        
-        /*System.out.println("Welcome to Turing!");
-        
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter username");
     
-        String userName = myObj.nextLine();  // Read user input
-        System.out.println("Username is: " + userName);  // Output user input*/
+    public static void main(String[] args ) throws InterruptedException {
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
+        boolean selectionValid = false;
+        while (selectionValid == false) {
+            System.out.println("        ##      ## ######## ##        ######   #######  ##     ## ########    ########  #######     ######## ##     ## ########  #### ##    ##  ######");   
+            System.out.println("        ##  ##  ## ##       ##       ##    ## ##     ## ###   ### ##             ##    ##     ##       ##    ##     ## ##     ##  ##  ###   ## ##    ##");  
+            System.out.println("        ##  ##  ## ##       ##       ##       ##     ## #### #### ##             ##    ##     ##       ##    ##     ## ##     ##  ##  ####  ## ##");        
+            System.out.println("        ##  ##  ## ######   ##       ##       ##     ## ## ### ## ######         ##    ##     ##       ##    ##     ## ########   ##  ## ## ## ##   ####"); 
+            System.out.println("        ##  ##  ## ##       ##       ##       ##     ## ##     ## ##             ##    ##     ##       ##    ##     ## ##   ##    ##  ##  #### ##    ##");  
+            System.out.println("        ##  ##  ## ##       ##       ##    ## ##     ## ##     ## ##             ##    ##     ##       ##    ##     ## ##    ##   ##  ##   ### ##    ##");  
+            System.out.println("        ###  ###  ######## ########  ######   #######  ##     ## ########       ##     #######        ##     #######  ##     ## #### ##    ##  ######");
+            System.out.println("    *********************************************************************************************************************************************************");
+            System.out.println("");
+            System.out.println("");   
+            System.out.println("");        
+            Thread.sleep(1500);       
         
-        return;   
+            System.out.println("Do you want to encrypt or decrypt a message?");
+            System.out.println("1 - Encrypt");   
+            System.out.println("2 - Decrypt");
+            Scanner myObj = new Scanner(System.in);
+            Thread.sleep(1000);
+            System.out.println("Enter Selection: ");
+            int selection = 0;
+            
+            if (myObj.hasNextInt()) {
+		        selection = myObj.nextInt();
+                if (selection == 1) {
+                    selectionValid = true;
+                    Thread.sleep(750);
+                    System.out.println("Your selection is: Encrypt");
+                    Thread.sleep(2000);
+                    System.out.print("\033[H\033[2J");  
+                    System.out.flush();
+                    Scanner scnr = new Scanner(System.in);  
+                    System.out.println("Enter message: ");
+                    String clearMessage = scnr.nextLine();
+                    Thread.sleep(750);  
+                    System.out.println("You entered: " + clearMessage);
+                } else if (selection == 2) {
+                    selectionValid = true;
+                    Thread.sleep(750);
+                    System.out.println("Your selection is: Decrypt");
+                    Thread.sleep(2000);
+                    System.out.print("\033[H\033[2J");  
+                    System.out.flush();
+                    Scanner scnr = new Scanner(System.in);  
+                    System.out.println("Enter message: ");
+                    String encryptedMessage = scnr.nextLine();
+                    Thread.sleep(750);  
+                    System.out.println("You entered: " + encryptedMessage);
+                } else {
+                    Thread.sleep(750);
+                    System.out.println("Your selection is invalid");
+                    Thread.sleep(2000);
+                    System.out.print("\033[H\033[2J");  
+                    System.out.flush();
+                }
+            } else {
+                Thread.sleep(750);
+                System.out.println("Your selection is invalid");
+                Thread.sleep(2000);
+                System.out.print("\033[H\033[2J");  
+                System.out.flush();
+            }
+		    
+		       
+             
+            
+        }
+           
     }
-        
-
-
-
-
 }
